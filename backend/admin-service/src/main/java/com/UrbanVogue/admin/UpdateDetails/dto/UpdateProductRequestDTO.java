610879904f2f121af1f4a3245e5f595d7ca493c2
gutti-likitha <gutti.likitha@mphasis.com>
2026-03-20 18:36:48 +0530

    UpdateProduct details DTO

---
+++ backend/admin-service/src/main/java/com/UrbanVogue/admin/UpdateDetails/dto/UpdateProductRequestDTO.java
@@ -0,0 +1,32 @@
+package com.UrbanVogue.admin.UpdateDetails.dto;
+
+public class UpdateProductRequestDTO {
+
+    private String name;
+    private Double price;
+    private String size;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+}
